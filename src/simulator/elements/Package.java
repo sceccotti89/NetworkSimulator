e5b3d09eea74acfad35d77111c7cd353dae4f8a3
cecco <cecco@192.168.1.106>
2017-03-22 19:25:59 +0100

    Updated.

--- src/simulator/elements/Package.java
+++ src/simulator/elements/Package.java
@@ -4,7 +4,13 @@ import org.newdawn.slick.Color;
 
 public class Package
 {
-	private String ID_From, ID_To;
+	private String ID_from, ID_to;
 	
 	private Color color;
+	
+	public Package( String ID_from, String ID_to, Color color ){
+		this.ID_from = ID_from;
+		this.ID_to = ID_to;
+		this.color = color;
+	}
 }
