b01b34d9ba53199c3abcfee9b66ddeeb55bd5dcc
cecco <cecco@192.168.1.106>
2017-04-17 19:49:29 +0200

    Updated.

--- src/simulator/elements/Link.java
+++ src/simulator/elements/Link.java
@@ -10,7 +10,7 @@ import org.newdawn.slick.geom.Rectangle;
 
 public class Link
 {
-	private float x1, x2, y1, y2;
+	//private float x1, x2, y1, y2;
 	
 	private Color color = Color.black;
 	
@@ -26,10 +26,10 @@ public class Link
 	private boolean showInfos = false;
 	
 	public Link( float x1, float y1, float x2, float y2, float angle ) {
-		this.x1 = x1;
+		/*this.x1 = x1;
 		this.x2 = x2;
 		this.y1 = y1;
-		this.y2 = y2;
+		this.y2 = y2;*/
 		
 		this.angle = angle;
 		
