b49eaad2e6553d76d6be3346b53e6fbae062b303
cecco <cecco@192.168.1.106>
2017-06-16 21:22:49 +0200

    Updated.

---
+++ src/simulator/test/GraphicTest.java
@@ -0,0 +1,25 @@
+package simulator.test;
+ 
+ import org.newdawn.slick.AppGameContainer;
+ import org.newdawn.slick.SlickException;
+ 
+ import simulator.graphics.AnimationNetwork;
+ 
+ public class GraphicTest
+ {
+     public static void main( final String[] args ) throws SlickException
+     {
+         AnimationNetwork an = new AnimationNetwork( 800, 600, "prova" );
+         AppGameContainer app = new AppGameContainer( an );
+         
+         an.loadSimulation( "data/File/Network.xml", "data/File/Packets.xml" );
+        
+         //an.addNode( 100, 200, 3, Color.red );
+         //an.addLink(  );
+         
+         app.setTargetFrameRate( 90 );
+         app.setDisplayMode( 800, 600, false );
+         
+         app.start();
+     }
+ } 
