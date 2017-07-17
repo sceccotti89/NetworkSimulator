0e02bd2dd9e49597b601cc12d2b46e522ea27abc
stefano <stefano@stefano>
2017-07-18 00:20:39 +0200

    Added network event generators.

---
+++ src/simulator/events/generator/SinkGenerator.java
@@ -0,0 +1,27 @@
+/**
+ * @author Stefano Ceccotti
+*/
+
+package simulator.events.generator;
+
+import simulator.events.Event;
+import simulator.events.Packet;
+import simulator.utils.Time;
+
+public class SinkGenerator extends EventGenerator
+{
+    public SinkGenerator( final Time duration,
+                          final long maxPacketsInFly,
+                          final Packet reqPacket,
+                          final Packet resPacket )
+    {
+        super( duration, Time.ZERO, maxPacketsInFly,
+               reqPacket, resPacket,
+               false, false, true );
+    }
+
+    @Override
+    public Time computeDepartureTime( final Event e ) {
+        return null;
+    }
+}
