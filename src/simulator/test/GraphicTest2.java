e3b1f823ad43451c4d98adb4c623a1b4f59f0af4
Stefano <Stefano@DESKTOP-BEQUTC5.fritz.box>
2017-08-10 10:22:19 +0200

    Updated.

--- src/simulator/test/GraphicTest2.java
+++ src/simulator/test/GraphicTest2.java
@@ -3,17 +3,16 @@ package simulator.test;
  
  import java.io.IOException;
 
-import simulator.graphics.plotter.parser.Evaluator;
 import simulator.graphics_swing.AnimationNetwork;
  
  public class GraphicTest2
  {
      public static void main( final String[] args ) throws IOException
      {
-         Evaluator evaluator = new Evaluator( "sqrt(2^10) + 5 - x^3" );
+         /*Evaluator evaluator = new Evaluator( "sqrt(2^10) + 5 - x^3" );
          evaluator.putVariable( "x", 3 );
          System.out.println( evaluator.eval() );
-         System.exit( 0 );
+         System.exit( 0 );*/
          
          AnimationNetwork an = new AnimationNetwork( 800, 600, "prova" );
          an.setTimer( 90 );
