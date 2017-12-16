f99387877048d004b2799305b7ef5fa769b5509d
Stefano <Stefano@DESKTOP-BEQUTC5.fritz.box>
2017-12-16 22:01:36 +0100

    Fixed.

--- src/simulator/test/energy/EnergyModel.java
+++ src/simulator/test/energy/EnergyModel.java
@@ -12,6 +12,7 @@ public abstract class EnergyModel
     private static final double Ps = 0.84d; //1.3d;
     /** Static power of a core. */
     private static final double Pind = 0.1d;
+    
     /** Alpha value used to evaluate the cpu power consumption. */
     private static final double alpha = 1.49;
     /** Beta value used to evaluate the cpu power consumption. */
@@ -23,25 +24,7 @@ public abstract class EnergyModel
      * 
      * @param _cores    number of cpu cores.
     */
-    public EnergyModel( double _cores )
-    {
-        /*energyIdleValues = new HashMap<>( 15 );
-        energyIdleValues.put( 3500000L, 4.3519141137124d / 4d );
-        energyIdleValues.put( 3300000L, 3.8033657190635d / 4d );
-        energyIdleValues.put( 3100000L, 3.7768193645485d / 4d );
-        energyIdleValues.put( 2900000L, 3.8801409698997d / 4d );
-        energyIdleValues.put( 2700000L, 2.8367161204013d / 4d );
-        energyIdleValues.put( 2500000L, 1.9872807357860d / 4d );
-        energyIdleValues.put( 2300000L, 1.7678120066890d / 4d );
-        energyIdleValues.put( 2100000L, 1.6599310033445d / 4d );
-        energyIdleValues.put( 2000000L, 1.4050869230769d / 4d );
-        energyIdleValues.put( 1800000L, 1.2006148494983d / 4d );
-        energyIdleValues.put( 1600000L, 1.1307208361204d / 4d );
-        energyIdleValues.put( 1400000L, 1.1299250167224d / 4d );
-        energyIdleValues.put( 1200000L, 1.1167856856187d / 4d );
-        energyIdleValues.put( 1000000L, 1.1034046822742d / 4d );
-        energyIdleValues.put(  800000L, 1.1007310702341d / 4d );*/
-        
+    public EnergyModel( double _cores ) {
         cores = _cores;
     }
     
